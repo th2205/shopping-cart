33202f4d0c161b3b683966942f5b3f93b3226966
th0566 <th0566@naver.com>
2020-06-03 21:42:33 +0900

    Complete Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <Item>임태현</Item>
+      <Item>
+        <HomeButton>Home</HomeButton>
+        <CartButton>장바구니</CartButton>
+      </Item>
+    </HeaderContainer>
+  );
+}
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 6vh;
+  border-bottom: 1px solid rgba(37, 38, 42, 0.2);
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 4px;
+  background-color: #ffffff;
+  margin-bottom: 20px;
+`;
+
+const Item = styled.li`
+  list-style: none;
+  margin: 0 50px;
+  padding: 0;
+`;
+
+const CartButton = styled.button`
+  width: 100px;
+  height: 30px;
+  border-radius: 5px;
+  font-size: 16px;
+  border: 0;
+  outline: none;
+  background-color: rgba(86, 229, 156, 0.9);
+  color: #ffffff;
+  cursor: pointer;
+`;
+
+const HomeButton = styled.button`
+  width: 100px;
+  height: 30px;
+  border-radius: 5px;
+  font-size: 16px;
+  border: 0;
+  outline: none;
+  background-color: rgb(197, 210, 221);
+  color: #ffffff;
+  cursor: pointer;
+  margin-right: 10px;
+`;
