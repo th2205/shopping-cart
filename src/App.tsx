b177b9353584e7f7c368ee175a6003de68d60ddf
th0566 <th0566@naver.com>
2020-06-03 21:12:59 +0900

    Add Home container, router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
+import Router from './routes';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <AppContainer>
+      <Router />
+    </AppContainer>
+  );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  background-color: rgb(249, 249, 249);
+`;
