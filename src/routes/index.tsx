3af8b55cbae8c14efcfb6ed1d4ae9c9d0c7afbd0
th0566 <th0566@naver.com>
2020-06-03 16:53:15 +0900

    Set react router

---
+++ src/routes/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+export default function Router() {
+  <BrowserRouter>
+    <Switch></Switch>
+  </BrowserRouter>;
+}
